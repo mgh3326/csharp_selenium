c4d496345639caedd68daed9fe210bc57577a5d3
mgh3326 <mgh3326@naver.com>
2018-04-03 14:42:21 +0900

    update

--- selenium_practice/Program.cs
+++ selenium_practice/Program.cs
@@ -12,11 +12,11 @@ namespace selenium_practice
     {
         static void Main(string[] args)
         {
-            //string oh = System.AppDomain.CurrentDomain.BaseDirectory;
+            //string oh = System.AppDomain.CurrentDomain.BaseDirectory;//실행 경로 확인하기 위해서 이렇게 했음
             //Console.WriteLine(oh);
-            IWebDriver driver = new ChromeDriver("..\\..\\");
+            IWebDriver driver = new ChromeDriver("..\\..\\");//Debug Release 다 가능하게 하기 위함
 
-            driver.Url = "https://www.youtube.com/?gl=KR&hl=ko";
+            driver.Url = "https://www.youtube.com/?gl=KR&hl=ko";//처음 링크 유튜브
         }
     }
 }
