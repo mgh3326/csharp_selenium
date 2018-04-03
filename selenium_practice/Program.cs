9359df2bf803a8e0843a133c3bbf60d8acabe0f8
mgh3326 <mgh3326@naver.com>
2018-04-03 14:33:18 +0900

    update

--- selenium_practice/Program.cs
+++ selenium_practice/Program.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+//using System.IO;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+
 namespace selenium_practice
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            IWebDriver driver = new ChromeDriver("C:\\chrome");
+            //string oh = System.AppDomain.CurrentDomain.BaseDirectory;
+            //Console.WriteLine(oh);
+            IWebDriver driver = new ChromeDriver("..\\..\\");
 
             driver.Url = "https://www.youtube.com/?gl=KR&hl=ko";
         }
